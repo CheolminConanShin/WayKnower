539fba2dcf15ba6d8c06077cd5a5ada5ee635676
CheolminConanShin <devcraft17@devcraft17s-iMac.local>
2016-12-22 11:49:13 +0900

    kakao link

--- src/javascript/route_select.js
+++ src/javascript/route_select.js
@@ -24,21 +24,14 @@ goThisWayButton.addEventListener("click", function(e) {
 });
 
 function activateKakao(){
-	alert("hell");
 	Kakao.init('3b1c9bd1870f46083d79ba8115f7f304');
 	// 카카오톡 링크 버튼을 생성합니다. 처음 한번만 호출하면 됩니다.
 	Kakao.Link.createTalkLinkButton({
 		container: '#kakao-link-btn',
-		label: '카카오링크 샘플에 오신 것을 환영합니다.',
-		image: {
-			src: 'http://dn.api1.kage.kakao.co.kr/14/dn/btqaWmFftyx/tBbQPH764Maw2R6IBhXd6K/o.jpg',
-			width: '300',
-			height: '200'
-		},
 		horizontalButton: [{
 			webButton: {
 				text: '카카오 디벨로퍼스',
-				url: 'https://dev.kakao.com/docs/js'
+				url: 'http://www.naver.com'
 			}
 		}, {
 			appButton: {
